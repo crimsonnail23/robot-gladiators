d60252a8e8d320d8dd7f16fb52fb512d72ed446e
night <yseddiq@gmail.com>
2022-06-21 15:00:23 -0400

    put in some basic javascript functions

---
+++ assets/js/game.js
@@ -0,0 +1,21 @@
+//this creates a function named "fight"
+
+var playerName = window.prompt("what is your robot's name?");
+
+console.log(playerName);
+
+console.log("this logs a string good for leaving yourself a message");
+
+//this will do math and lot 20
+
+console.log(10 + 10);
+
+console.log("our robot's name is " + playerName);
+
+function fight() {
+
+window.alert("The Fight Has Begun");
+
+}
+
+// fight();
