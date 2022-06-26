13f553846f9071c1509158001d7a15a441d080bb
night <yseddiq@gmail.com>
2022-06-26 16:12:37 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,6 +28,14 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 
+var randomNumber = function(min, max) {
+
+    var value=Math.floor(Math.random() * (max - min +1)+ min);
+
+    return value;
+
+}
+
 
 var fight = function(enemyName) {
 
@@ -55,7 +63,7 @@ var fight = function(enemyName) {
     
         //subtract money from player money
         
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0,playerMoney - 10);
     
         console.log("playerMoney ", playerMoney);
     
@@ -66,12 +74,12 @@ var fight = function(enemyName) {
     }
 
    
+//creates a random value for the damage.
+        var damage = randomNumber(playerAttack -3, playerAttack);
 
-   
+    
 
-    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-
-        enemyHealth = enemyHealth - playerAttack;
+        enemyHealth = Math.max(0, enemyHealth - damage);
 
     // Log a resulting message to the console so we know that it worked.
 
@@ -101,9 +109,9 @@ var fight = function(enemyName) {
 
     }
 
-    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+           var damage = randomNumber(enemyAttack -3, enemyAttack) 
 
-            playerHealth = playerHealth - enemyAttack;
+            playerHealth = Math.max(0, playerHealth - damage);
 
     // Log a resulting message to the console so we know that it worked.
 
@@ -162,7 +170,7 @@ for(var i=0; i < enemyNames.length; i++){
 
         var pickedEnemyName=enemyNames[i];
 
-        enemyHealth=50;
+        enemyHealth=randomNumber(40,60);
 
         fight(pickedEnemyName);
 
